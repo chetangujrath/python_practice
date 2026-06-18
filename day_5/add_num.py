90a3b91c961a5475166f9cb8304184fe4bb04f29
chetangujrath <163118410+chetangujrath@users.noreply.github.com>
2026-06-18 15:19:21 +0530

    Initial commit of python practice code

---
+++ day_5/add_num.py
@@ -0,0 +1,4 @@
+def add_two_num():
+    num1 = int(input("enter 1 number:"))
+    num2 = int(input("enter 2 number:"))
+    print(num1 + num2)
--- day_5/call_fun.py
+++ day_5/call_fun.py
@@ -1,5 +1,15 @@
 from linear_search_fun import linear_search
+from utils import to_uppercase
+from utils import check_even_odd
+from add_num  import add_two_num
 
 found = linear_search(["jaksh","Piyush","Jwal","Chetan"],"Jwal")
 
 print(found)
+
+print(to_uppercase("Hello chetan gujrathi"))
+
+result = check_even_odd(36)
+print(result)
+
+add_two_num()
