bb61dbe67e8ab45af80106c54000aba405fa4d48
chetangujrath <163118410+chetangujrath@users.noreply.github.com>
2026-06-18 15:25:10 +0530

    added substraction code

--- day_5/add_num.py
+++ day_5/add_num.py
@@ -2,3 +2,8 @@ def add_two_num():
     num1 = int(input("enter 1 number:"))
     num2 = int(input("enter 2 number:"))
     print(num1 + num2)
+
+def sub_two_num():
+    number1 = int(input("Enter first number:"))
+    number2 = int(input("Enter second number:"))
+    print(number1 - number2)
--- day_5/call_fun.py
+++ day_5/call_fun.py
@@ -2,6 +2,7 @@ from linear_search_fun import linear_search
 from utils import to_uppercase
 from utils import check_even_odd
 from add_num  import add_two_num
+from add_num import sub_two_num
 
 found = linear_search(["jaksh","Piyush","Jwal","Chetan"],"Jwal")
 
@@ -13,3 +14,5 @@ result = check_even_odd(36)
 print(result)
 
 add_two_num()
+
+sub_two_num()
