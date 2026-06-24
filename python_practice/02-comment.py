03b4025af3d262fc0c7421f200525dc588073ea2
chetangujrath <163118410+chetangujrath@users.noreply.github.com>
2026-06-24 14:18:13 +0530

    added new practice code

---
+++ python_practice/02-comment.py
@@ -0,0 +1,16 @@
+print("This is the comment practice code in this programm")
+
+print("In python we will comment the code by '#' key in start of the line")
+
+# print("This is the comment single line in python")
+print("\n")
+print("This is the multiple line commment like this ")
+
+print("In python we will comment the code by key in start of the line")
+print("\n")
+      
+"""
+this is multiple line commment in python 
+
+"""
+
---
+++ python_practice/03-variable.py
@@ -0,0 +1,50 @@
+print("Normal variable example :- ")
+a = "Hello World"
+print(a)
+print("\n")
+print("*****************************************************")
+
+print("Name and age variable example :-")
+name = "Chetan"
+age = 39
+print(f"My name is {name} & My age is {age}")
+print("My name is" + name + ". My age is",age) #varible concatinate 
+
+print("\n")
+print("*****************************************************")
+
+print("Multiple variables example :- ")
+fname = "Vaibhav"
+lname = "Suryavanshee"
+age = 15
+profession = "cricketor"
+address = "Bihar"
+
+print(f"My name is {fname} {lname}")
+print(f"I am {age} years old & I am from {address}")
+print(f"I am under 19 {profession}")
+
+print("\n")
+print("*****************************************************")
+
+print("Multiple variable in single line")
+print("Numeric value example")
+a,b,c = 10,20,30
+print(a)
+print(b)
+print(c)
+print("String value example")
+x,y,z = "Pune","Mumbai","Delhi"
+print(x)
+print(y)
+print(z)
+
+print("\n")
+print("*****************************************************")
+
+print("Multiple variable and single value ")
+
+p = q = r = 50
+print(p)
+print(q)
+print(r)
